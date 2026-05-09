5e086758446923c69f39145cbc64bdeff0e3e4a3
Chadith Dissanayaka <chadithdissanayaka@gmail.com>
2026-05-09 18:16:42 +0530

    Fixed Wrong nameing

--- backend/organDetection-service/run.py
+++ backend/organDetection-service/run.py
@@ -11,7 +11,7 @@ load_dotenv()
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(
-        "organ-detection:app",
+        "main:app",
         host="0.0.0.0",
         port=port,
         reload=False,   # Set to True during local development
